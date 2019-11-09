f2f91e0a5a2892ac8db3bdf33c3281e0eea97045
jetpackpony <jetpackpony@gmail.com>
2019-11-09 19:49:59 +0300

    Add a transport class

---
+++ src/MTProtoTransport/index.js
@@ -0,0 +1,52 @@
+class Abridged {
+  initialByteSent = false;
+
+  packMessage(bytes) {
+    const header = [];
+    if (!this.initialByteSent) {
+      this.initialByteSent = true;
+      header.push(0xef);
+    }
+
+    const len = bytes.length / 4;
+    if (len >= 127) {
+      header.push(127);
+      header.push(len & 0xff);
+      header.push((len >> 8) & 0xff);
+      header.push((len >> 16) & 0xff);
+    } else {
+      header.push(len);
+    }
+    
+    const buf = new ArrayBuffer(header.length + bytes.length);
+    const uint8 = new Uint8Array(buf);
+    uint8.set(header, 0);
+    uint8.set(bytes, header.length);
+
+    return uint8;
+  }
+
+  unpackMessage(data) {
+    const uint8 = new Uint8Array(data);
+    console.log('data recieved: ', uint8.toString());
+
+    let len, offset;
+    if (uint8[0] === 127) {
+      len = uint8[1] * 1 + uint8[2] * 256 + uint8[3] * 4096;
+      offset = 4;
+    } else {
+      len = uint8[0];
+      offset = 1;
+    }
+    const msg = uint8.slice(offset);
+
+    if ((len * 4 + offset) !== data.length) {
+      console.log(`Data is corrupt: proclaimed length is ${len}, actual: ${data.length} `);
+      return;
+    }
+
+    return msg;
+  }
+}
+
+module.exports = { Abridged };
