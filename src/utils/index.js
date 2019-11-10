d30c0bf1925894d7e15a43ec1d06e0f947d1ef1c
jetpackpony <jetpackpony@gmail.com>
2019-11-10 14:27:47 +0300

    Add utils for making aes keys

---
+++ src/utils/index.js
@@ -0,0 +1,31 @@
+const { bytesToSHA1 } = require("../crypto");
+
+const makeTmpAESKeys = (newNonce, serverNonce) => {
+  const newNoncePlusServer = bytesToSHA1(concatUint8([newNonce, serverNonce]));
+  const serverPlusNewNonce = bytesToSHA1(concatUint8([serverNonce, newNonce]));
+  const newNoncePlusNewNonce = bytesToSHA1(concatUint8([newNonce, newNonce]));
+
+  const tmp_aes_key = Uint8Array.from(newNoncePlusServer.concat(serverPlusNewNonce.slice(0, 12)));
+  const tmp_aes_iv = concatUint8([
+    Uint8Array.from(serverPlusNewNonce.slice(12, 20).concat(newNoncePlusNewNonce)),
+    newNonce.slice(0, 4)
+  ]);
+
+  return [tmp_aes_key, tmp_aes_iv];
+}; 
+
+const concatUint8 = (listOfArrays) => {
+  const len = listOfArrays.reduce((sum, arr) => {
+    sum += arr.length;
+    return sum;
+  }, 0);
+  const uint8 = new Uint8Array(new ArrayBuffer(len));
+  let offset = 0;
+  listOfArrays.forEach((arr) => {
+    uint8.set(arr, offset);
+    offset += arr.length;
+  });
+  return uint8;
+};
+
+module.exports = { makeTmpAESKeys };
