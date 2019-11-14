bc8ce15aacafc69968c84d2ee4d61bd775637ec3
jetpackpony <jetpackpony@gmail.com>
2019-11-14 14:19:31 +0300

    Make echoserver use websockets

--- src/echoServer.js
+++ src/echoServer.js
@@ -1,11 +1,56 @@
-const net = require('net');
 const HOST = '127.0.0.1';
 const PORT = 3000;
 
-const server = net.createServer(socket => {
-  socket.on('data', (data) => {
-    console.log('Echoing: %s', data.toString())
-    socket.write(data.toString())
-  })
+// const net = require('net');
+// const server = net.createServer(socket => {
+//   socket.on('data', (data) => {
+//     console.log('Echoing: %s', data.toString())
+//     socket.write(data.toString())
+//   })
+// });
+// server.listen(PORT);
+
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+ 
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
 });
-server.listen(PORT);
+server.listen(PORT, function() {
+    console.log((new Date()) + ' Server is listening on port 3000');
+});
+ 
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+ 
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+ 
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Incoming binary' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
